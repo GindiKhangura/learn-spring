7a04aaa3ae352e3a755b5ffe5bb95f48a6b0125f
Gindi Khangura <gindi.khangura@cerner.com>
2018-08-09 15:30:06 -0500

    Just a dummy commit to test if I have to supply the upstream origin again or not.

--- gs-rest-service/src/main/java/hello/Application.java
+++ gs-rest-service/src/main/java/hello/Application.java
@@ -8,6 +8,8 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		// Just some dummy comment to try pushing to branch, hopefully I do not have to
+		// supply the upstream origin thing again.
 	}
 
 }
